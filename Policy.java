2c874f63a8457f81317942d8754425171a9259d6
Samir M. <samirminaya2@gmail.com>
2025-09-19 22:34:24 -0400

    Update Policy.java
    Added code for Policy class.

---
+++ Policy.java
@@ -0,0 +1,30 @@
+/*
+ * This is a policy class that will model an insurance policy for one person
+ * This will include  
+ * Policy
+ *  Number; First & Last Name; Age; Smoking Status; Height; Weight - Class fields
+ * 
+ * No arg Constructor 
+ * arg Constructor
+ * 
+ * Each field will have both a getter and setter method
+ * 
+ * Method that calculates and returns the BMI
+ *  BMI = (Weight[pound] * 703) / (Height^2[inch])
+ * 
+ * Base - 600
+ * age 50> additional 75 fee
+ * isSmoker additional 100 fee
+ * BMI 35> additional fee -
+ *  - AddFee = (BMI - 35) * 20 {20 per index over 35}
+ * 
+ * AVOID STALE DATA (BMI)
+ */
+
+public class Policy
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
